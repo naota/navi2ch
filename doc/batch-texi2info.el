26e5d27ff8a5614603756c2a7d0bf890d297b3a9
nanashi <nanashi>
2002-01-06 16:28:19 +0000

    initial checkin

---
+++ doc/batch-texi2info.el
@@ -0,0 +1,97 @@
+;;; batch-texi2info.el --- run texi2info with emacs -batch
+
+;; Copyright (C) 1985, 1986, 1988, 1990, 1991, 1992, 1993, 
+;;               1994, 1995, 1996, 1997, 1998, 2000, 2001
+;;    Free Software Foundation, Inc.
+;; Copyright (C) 2002 by 2ちゃんねる
+
+;; Author: 名無しさん＠お腹いっぱい
+;; Keywords: network, 2ch
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'texinfmt)
+
+(set-language-environment "Japanese")
+
+;; batch-texinfo-format からほぼコピペ
+(defun batch-texi2info ()
+  "Runs  texi2info  on the files remaining on the command line.
+Must be used only with -batch, and kills emacs on completion.
+Each file will be processed even if an error occurred previously.
+For example, invoke
+  \"emacs -batch -funcall batch-texi2nfo $docs/ ~/*.texinfo\"."
+  (if (not noninteractive)
+      (error "batch-tex2info may only be used -batch"))
+  (if (< emacs-major-version 21)
+      (error "Please use Emacs 21 or later"))
+  (let ((version-control t)
+        (auto-save-default nil)
+        (find-file-run-dired nil)
+        (kept-old-versions 259259)
+        (kept-new-versions 259259)
+	(coding-system-for-read 'undecided)
+	(coding-system-for-write 'iso-2022-7bit-unix))
+    (let ((error 0)
+          file
+          (files ()))
+      (while command-line-args-left
+        (setq file (expand-file-name (car command-line-args-left)))
+        (cond ((not (file-exists-p file))
+               (message ">> %s does not exist!" file)
+               (setq error 1
+                     command-line-args-left (cdr command-line-args-left)))
+              ((file-directory-p file)
+               (setq command-line-args-left
+                     (nconc (directory-files file)
+                            (cdr command-line-args-left))))
+              (t
+               (setq files (cons file files)
+                     command-line-args-left (cdr command-line-args-left)))))
+      (while files
+        (setq file (car files)
+              files (cdr files))
+        (condition-case err
+            (progn
+              (if buffer-file-name (kill-buffer (current-buffer)))
+              (find-file file)
+              (buffer-disable-undo (current-buffer))
+              (set-buffer-modified-p nil)
+              (texinfo-mode)
+              (message "texinfo formatting %s..." file)
+              (texi2info nil)
+              (if (buffer-modified-p)
+                  (progn (message "Saving modified %s" (buffer-file-name))
+                         (save-buffer))))
+          (error
+           (message ">> Error: %s" (prin1-to-string err))
+           (message ">>  point at")
+           (let ((s (buffer-substring (point)
+                                      (min (+ (point) 100)
+                                           (point-max))))
+                 (tem 0))
+             (while (setq tem (string-match "\n+" s tem))
+               (setq s (concat (substring s 0 (match-beginning 0))
+                               "\n>>  "
+                               (substring s (match-end 0)))
+                     tem (1+ tem)))
+             (message ">>  %s" s))
+           (setq error 1))))
+      (kill-emacs error))))
