4b12eed169dfb84229a2e9497bc337f52efc6730
nanashi <nanashi>
2002-02-13 00:13:50 +0000

    change bbstable location.

--- navi2ch-vars.el
+++ navi2ch-vars.el
@@ -159,7 +159,7 @@
   :type 'boolean
   :group 'navi2ch-list)
 
-(defcustom navi2ch-list-bbstable-url "http://www.2ch.net/newbbsmenu.html"
+(defcustom navi2ch-list-bbstable-url "http://www.2ch.net/2chmenu.html"
   "*bbstable の URL。"
   :type 'string
   :group 'navi2ch-list)
