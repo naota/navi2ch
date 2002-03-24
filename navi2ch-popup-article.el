d98d1d6c44c365a22a1fd069b480e5203bccb1dc
mami <mami>
2002-03-24 08:18:54 +0000

    navi2ch-popup-article-mode-map: remove M-b binding

--- navi2ch-popup-article.el
+++ navi2ch-popup-article.el
@@ -52,7 +52,7 @@
     (define-key map ">" 'navi2ch-article-goto-last-message)
     (define-key map "<" 'navi2ch-article-goto-first-message)
     (define-key map "\eu" 'navi2ch-article-uudecode-message)
-    (define-key map "\eb" 'navi2ch-article-base64-decode-message)
+    (define-key map "\ed" 'navi2ch-article-base64-decode-message)
     (define-key map "v" 'navi2ch-article-view-aa)
     (setq navi2ch-popup-article-mode-map map)))
     
