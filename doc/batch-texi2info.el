67e89d2ce276e66b00b73ccc524c50a5751f5a14
nanashi <nanashi>
2002-07-10 12:06:37 +0000

    can be configured and built in another directory.

--- doc/batch-texi2info.el
+++ doc/batch-texi2info.el
@@ -1,6 +1,6 @@
 ;;; batch-texi2info.el --- run texi2info with emacs -batch
 
-;; Copyright (C) 1985, 1986, 1988, 1990, 1991, 1992, 1993, 
+;; Copyright (C) 1985, 1986, 1988, 1990, 1991, 1992, 1993,
 ;;               1994, 1995, 1996, 1997, 1998, 2000, 2001
 ;;    Free Software Foundation, Inc.
 ;; Copyright (C) 2002 by 2ちゃんねる
