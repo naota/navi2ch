e538b3c40a5fea1bb8a08e7cd367e52e74b03791
mami <mami>
2004-01-29 16:25:19 +0000

    contrib/navi2ch-spamfilter.el: add license term

--- contrib/navi2ch-spamfilter.el
+++ contrib/navi2ch-spamfilter.el
@@ -5,7 +5,20 @@
 ;; Author: http://pc.2ch.net/test/read.cgi/unix/1065246418/38
 ;; Keywords: 2ch, network, spam
 
-;; (LICENSE TERMS TO BE FILLED)
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
 
 ;;; Commentary:
 
