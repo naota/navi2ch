876c4032abaeb592e31f78d0273bc8365a6f9f59
Naohiro Aota <naota@elisp.net>
2010-09-17 17:10:35 +0900

    navi2ch-thumbnail: Add Commentaries.
    ファイル冒頭のコメントや著作権表示の追加。

--- navi2ch-thumbnail.el
+++ navi2ch-thumbnail.el
@@ -1,4 +1,26 @@
-(provide 'navi2ch-thumbnail)
+;;; navi2ch-thumbnail.el --- thumbnail view for navi2ch
+
+;; Copyright (C) 2010 by Navi2ch Project
+
+;; Authors: MIZUNUMA Yuto <mizmiz@users.sourceforge.net>
+;; Keywords: network 2ch
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
 
 ;; サムネイルを表示する機能です
 ;; 画像表示に対応したemacsで動きます
@@ -27,6 +49,10 @@
 ;;   (setq navi2ch-browse-url-image-program "/Applications/Preview.app/Contents/MacOS/Preview")
 ;;   (setq navi2ch-thumbnail-image-convert-program "/opt/local/bin/convert") ;;MacPort ImageMagick 
 ;;   (setq navi2ch-thumbnail-image-identify-program "/opt/local/bin/identify")
+
+;;; Code
+
+(provide 'navi2ch-thumbnail)
 
 ;;使い方、兼キーバインド
 
