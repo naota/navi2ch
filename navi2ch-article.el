c7285252478a66e642e08522a8ffaeb92cec4760
MIZUNUMA Yuto <mizmiz@sf.net>
2011-10-08 01:31:15 +0900

    thread stop

--- navi2ch-article.el
+++ navi2ch-article.el
@@ -1430,7 +1430,7 @@ FIRST が nil ならば、ファイルが更新されてなければ何もしない。"
 						   navi2ch-article-view-range))))
 	    (navi2ch-article-load-number)
 	    (navi2ch-article-save-info board article first)
-        (navi2ch-article-thread-stop-p)
+            (navi2ch-article-thread-stop-p)
 	    (navi2ch-article-set-mode-line)
 	    (run-hooks 'navi2ch-article-after-sync-hook)
 	    list)
@@ -1648,22 +1648,12 @@ FIRST が nil ならば、ファイルが更新されてなければ何もしない。"
    navi2ch-article-mode-line-format t t t))
 
 (defun navi2ch-article-thread-stop-p ()
-;  (interactive)
-  (let ((suresuto nil))
-    (if (cdr (assq 'stop navi2ch-article-current-article))
-        ;;スレストフラグの付いたスレ
-        (setq suresuto t)
+  (if (cdr (assq 'stop navi2ch-article-current-article))
+      ;;スレストされているスレ(日付欄=停止で判断)
       (when (string-match "^停止" (cdr (assq 'date (navi2ch-article-get-message (length navi2ch-article-message-list)))))
-;        (message "スレストされてます by search")
-        (setq suresuto t)
         (setq navi2ch-article-current-article
               (navi2ch-put-alist 'stop t navi2ch-article-current-article))
-        (navi2ch-article-save-info navi2ch-article-current-board navi2ch-article-current-article)
-        ))
-;    (when suresuto
-;      (navi2ch-article-set-mode-line)
-;      (message "スレストされてます by navi2ch-article-current-article"))
-    ))
+        (navi2ch-article-save-info navi2ch-article-current-board navi2ch-article-current-article))))
 
 (defun navi2ch-article-sync-disable-diff (&optional force)
   (interactive "P")
@@ -1784,11 +1774,13 @@ FIRST が nil ならば、ファイルが更新されてなければ何もしない。"
 
 (defun navi2ch-article-write-message (&optional sage cite)
   (interactive)
+  (if (cdr (assq 'stop navi2ch-article-current-article))
+      (error "スレストされたスレには書き込めません"))
   (when (not (navi2ch-board-from-file-p navi2ch-article-current-board))
     (navi2ch-article-save-number)
     (navi2ch-message-write-message navi2ch-article-current-board
                                    navi2ch-article-current-article
-				   nil sage cite)))
+                                   nil sage cite)))
 
 (defun navi2ch-article-write-sage-message ()
   (interactive)
