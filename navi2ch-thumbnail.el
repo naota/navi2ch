39e6aa1700262505bb19ee07481e9d053a083274
MIZUNUMA Yuto <mizmiz@sf.net>
2015-03-30 02:43:20 +0900

    twitter use https

--- navi2ch-thumbnail.el
+++ navi2ch-thumbnail.el
@@ -193,7 +193,7 @@
                "GET"))
         cont)
     (setq cont (navi2ch-net-get-content proc))
-    (if (string-match "src=\"\\(http://pbs\.twimg\.com/media/.+\.jpg\\)\"" cont)
+    (if (string-match "src=\"\\(https?://pbs\.twimg\.com/media/.+\.jpg\\)\"" cont)
         (setq twitpic-img (match-string 1 cont))
       (error "can't get image url from %s" url))))
 
